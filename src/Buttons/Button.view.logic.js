7a78ed6820739e5c8063ca18f29a81df43d41e02
Darío Javier Cravero <dario@uxtemple.com>
2019-04-13 17:18:35 +0100

    feat: selectable buttons

--- src/Buttons/Button.view.logic.js
+++ src/Buttons/Button.view.logic.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button.view.js'
 
-let ButtonLogic = props => (
-  <Button
-    {...props}
-    onClick={() => {
-      if (typeof props.onClick === 'function') {
-        props.onClick(props.id)
-      }
-    }}
-  />
-)
+let ButtonLogic = props => {
+  let [isSelected, setIsSelected] = useState(false)
+
+  return (
+    <Button
+      {...props}
+      isSelected={isSelected}
+      onClick={() => {
+        setIsSelected(!isSelected)
+
+        if (typeof props.onClick === 'function') {
+          props.onClick(props.id)
+        }
+      }}
+    />
+  )
+}
 
 export default ButtonLogic
